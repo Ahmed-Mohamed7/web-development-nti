d66c7ac565fb27d7390c93d7c3136c477a10487c
Ahmed-Mohamed7 <53240428+Ahmed-Mohamed7@users.noreply.github.com>
2022-02-07 20:13:58 +0200

    Update task2.php

--- task2.php
+++ task2.php
@@ -2,30 +2,27 @@
 
 declare(strict_types=1);
 
-function GetNextChar(string $char):string
+function GetNextChar(string $char): string
 {
     $a = ord('a');
     $A = ord('A');
-    
-    if($char>='a' && $char<='z')
+
+    if ($char >= 'a' && $char <= 'z') 
     {
         #lower case
-        return chr(( (ord($char) - $a) + 1) % 26 + $a); 
-    }
-    else if ($char>='A' && $char<='Z')
+        return chr(((ord($char) - $a) + 1) % 26 + $a);
+    } 
+    else if ($char >= 'A' && $char <= 'Z') 
     {
         #upper case
-        return chr(( (ord($char) - $A) + 1) % 26 + $A); 
+        return chr(((ord($char) - $A) + 1) % 26 + $A);
     }
     return '-1';
 }
 
-$character =readline('Enter character: ');
+$character = readline('Enter character: ');
 $result = GetNextChar($character);
-if($result == '-1')
+if ($result == '-1')
     echo "you should enter a letter a-z A-Z\n";
-else 
+else
     echo $result;
-
-
-?>
