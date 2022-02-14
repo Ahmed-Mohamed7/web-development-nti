1b8d64a67cc664593767af096886c2ff78754d1d
Ahmed-Mohamed7 <53240428+Ahmed-Mohamed7@users.noreply.github.com>
2022-02-14 22:24:30 +0200

    modify delete image

--- week2/task2/delete.php
+++ week2/task2/delete.php
@@ -4,15 +4,18 @@ require_once 'connections.php';
 
 $id = $_GET['id'];
 
-$sql = "delete from posts where id = $id";
-
-$op = mysqli_query($conn, $sql);
-
 $image_sql = "select image from posts where id = $id";
 $image = mysqli_query($conn, $image_sql);
-$image = mysqli_fetch_assoc($image);
+
+$sql = "delete from posts where id = $id";
+$op = mysqli_query($conn, $sql);
+
+
+$image_date = mysqli_fetch_assoc($image);
+
 if ($op) {
-    unlink($image);
+   // echo $image_date['image'];
+    unlink($image_date['image']);
     $Message =  'posts deleted successfully';
 } else {
     $Message = 'Error Try Again';
